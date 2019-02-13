d8f8cd055db70db3d0a8870f6fdbda17a7f87e0e
Seung Min <smkim@nuriggumsoft.com>
2019-02-13 14:30:53 +0900

    CostFunction

---
+++ CostFunction/CostFunction.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 28 15:47:48 2019
+
+@author: sapzzil
+"""
+import tensorflow as tf
+
+def mse(model,y):
+    return tf.reduce_mean(tf.square(model-y))
+    
